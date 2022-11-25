d6da6f4aee7fff35592f22e7c3754fc72f2b85e4
Audrey <hello@rainy.garden>
2022-11-25 06:03:59 -0500

    add a resource compiler!

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,30 @@
 #include "core/engine.h"
 
+#include <iostream>
+#include <cmrc/cmrc.hpp>
+
+CMRC_DECLARE(tea);
+
+void res_fs_iterate(cmrc::embedded_filesystem res_fs, int depth, const std::string& path) { // NOLINT(misc-no-recursion)
+    for (auto content : res_fs.iterate_directory(path)) {
+        std::cout << std::string(depth * 2, ' ');
+        if (content.is_directory()) {
+            std::cout << content.filename() << "/" << std::endl;
+            res_fs_iterate(res_fs, depth + 1, path + "/" + content.filename());
+        } else {
+            std::cout << content.filename() << std::endl;
+        }
+    }
+}
+
+void res_test() {
+    auto res_fs = cmrc::tea::get_filesystem();
+    res_fs_iterate(res_fs, 0, "");
+}
+
 int main(int argc, char* argv[]) {
+    res_test();
+
     // collate program arguments into a vector of strings, excluding the first argument
     // (since the first argument is the launch path, which we don't want to expose to projects)
     std::vector<std::string> args(argv + 1, argv + argc);
